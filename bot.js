3c0a7016b2e167758da6ca8d17c0e574cbce2404
1Spokii1 <60269525+1Spokii1@users.noreply.github.com>
2020-04-16 16:24:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const client = new Discord.Client();
 const express = require('express');
+const db = require('quick.db');
 const keepalive = require('express-glitch-keepalive');
 const app = express();
 app.use(keepalive);
@@ -87,13 +89,163 @@ bot.on('message', async message => {
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.on('messageDelete', async (message) => {
+        const logs = message.guild.channels.find(channel => channel.name === "logs");
+        if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
+          message.guild.createChannel('logs', 'text');
+          //NOW THE BOT WILL CREATE A CHANNEL
+        }
+        if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) { 
+          console.log('There is no channel nammed "logs" and i don\'t have permission to create one')
+        }  
+        const entry = await message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE'}).then(audit => audit.entries.first())
+        let user = ""
+          if (entry.extra.channel.id === message.channel.id
+            && (entry.target.id === message.author.id)
+            && (entry.createdTimestamp > (Date.now() - 5000))
+            && (entry.extra.count >= 1)) {
+          user = entry.executor.username
+        } else { 
+          user = message.author.username
+        }
+        logs.send({embed: {
+          color: 0xb30b3d,
+          author: {
+          },
+          title: "MESSAGE DELETED",
+          url: "",
+          description: "",
+          fields: [{
+              name: "**Удаленное сообщение**",
+              value: (`${message.content}`)
+            },
+            {
+              name: "**Отправитель**",
+              value: (`${message.author}`)
+            },
+            {
+              name: "**Удалено в**",
+              value: (`${message.channel.name}`)
+            },
+            {
+              name: "**Удалил**",
+              value: (`${user}`)
+            },
+          ],
+          timestamp: new Date(),
+          footer: {
+            text: "logs"
+      
+          }}})})
+
+
+
+
+ bot.on('messageUpdate', function(oldMessage, newMessage) {
+
+        if (newMessage.channel.type == 'text' && newMessage.cleanContent != oldMessage.cleanContent) {
+  
+    var log = newMessage.guild.channels.find('name', "logs");
+          if (log != null)
+              log.sendMessage({embed: {
+        color: 0xb30b3d,
+        author: {
+        },
+        title: "MESSAGE UPDATE",
+        url: "",
+        description: "",
+        fields: [{
+            name: "Отправитель",
+            value: (`${newMessage.author}`)
+          },
+          {
+            name: "Старое сообщение ",
+            value: (`${oldMessage.cleanContent}`)
+          },
+          {
+            name: "Измененное сообщение ",
+            value: (`${newMessage.cleanContent}`)
+          },
+        ],
+        timestamp: new Date(),
+        footer: {
+          text: "logs"
+        }
+      }
+      });
+      }
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("message", async (message) => {
+  
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+    
+  let args = message.content.slice(prefix.length).trim().split(" ");
+  let cmd = args.shift().toLowerCase();
+  if (!message.content.startsWith(prefix)) return;
+  
+  try {
+            let commandFile = require(`./commands/${cmd}.js`);
+            commandFile.run(client, message, args);
+
+        } catch (e) {
+
+            console.log(e);
+
+        }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 bot.on('ready', () => {
     bot.user.setStatus('available')
     bot.user.setPresence({
         game: {
             name: '/ - prefix',
             type: "Playing",
-            url: "https://www.twitch.tv/bbuffer_"
+            url: "https://www.twitch.tv/"
         }
     });
 });
